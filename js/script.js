518c24b36ebd989603cf94f7f37b983e250fe95c
danya <danya.leonov199014@gmail.com>
2025-06-17 03:10:35 +0300

    zaebalo nahuy

--- js/script.js
+++ js/script.js
@@ -32,42 +32,30 @@ document.getElementById('myForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
     const formData = new FormData(this);
-    const data = {
-        'preset_id': 443,  // Укажите нужный preset_id
-        'type': 'mysql',   // Укажите нужный тип базы данных
-        'name': 'diplom',  // Установите имя базы данных как "diplom"
-        'description': formData.get('description') || 'описание вашей базы данных' // Получите описание из формы, если оно есть
-    };
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
 
-    const url = 'https://api.timeweb.cloud/api/v1/databases';  // URL API
-
-    fetch(url, {
+    fetch('http://103.74.93.220:8080/submit-form', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjFrYnhacFJNQGJSI0tSbE1xS1lqIn0.eyJ1c2VyIjoid3k0MDU4MSIsInR5cGUiOiJhcGlfa2V5IiwiYXBpX2tleV9pZCI6IjBhNGIwM2RjLWI3ZmMtNGRmNC1iMDYwLTc0M2U5MjZjYjdmMCIsImlhdCI6MTc1MDExNzMyN30.kYyE_1oA2JaH2b69zRr6ct-fCaiZLrkW0-8-p_YhLp_V9ZqASUPkSPPg4-mNIHW-qNj5X37SgJFf2zTDSrZqFOEKhRRlKRam6Qtlrc0zBgs3G3W7WxBLHDG8KOvReAar6lW15vRmY8MFHHLVZaI9uuoJQVvh0i83Cxp3KYlA_j24p9RZ_hwOWUKCT4pIHAU0VSYBuBvnumEW4ARmWGXZ6WyLyLOTJE4s4ztsiwS6qIcDZ8QKobAu-qomKSf9eex9Yx2PdueQmNS2wJU9_nXqdIe25ww9WF-L4MRjp4JwVZxRb4Xk-dvFx_ix09uWEn_graBTsKiVTAPNyU5EvUjbLxj8nNWWOlTJqoKjkOxhYcHMmMz8wiHfNqvsk5iVQGDVPi-3rqe1IYknlxjChcaBgSnkm9dpPZVIJRGGIMUimuy_3IJgzxOi5pN8-kypNN4wrr8yrAaLEmN4bWMuxL0SeLrZkVqoHcsRhaonj0OoHqZZDjgdylQGrOjzqfdKd1Rm
-    }`,  // Убедитесь, что токен установлен
-            'Accept': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     })
         .then(response => {
             if (response.ok) {
-                return response.json(); // Parse the JSON response
+                alert('Сообщение отправлено!');
+                document.getElementById('myForm').reset();
             } else {
-                return response.json().then(errorData => { // Parse error response
-                    throw new Error(`Ошибка отправки сообщения! Status: ${response.status}, Details: ${JSON.stringify(errorData)}`);
-                });
+                alert('Ошибка отправки сообщения!');
             }
-        })
-        .then(responseData => {
-            console.log('Success:', responseData);  // Log the successful response
-            alert('Сообщение отправлено!');
-            document.getElementById('myForm').reset();
         })
         .catch(error => {
             console.error('Ошибка:', error);
-            alert('Произошла ошибка при отправке: ' + error.message);
+            alert('Произошла ошибка при отправке!');
         });
 });
+
 
